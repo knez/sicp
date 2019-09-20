43abf9353b26f33424f3199496c4057830a0e3e5
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-09-20 21:18:40 +0200

    Add 1.07

---
+++ chapter1/1.07.scm
@@ -0,0 +1,21 @@
+; Simple test will fail for small numbers like 0.001. The threshold in the
+; testing function is not small enough, as a result, giving inaccurate
+; approximation. For really large numbers, like 1e+13, the resulting
+; difference in the good-enough? function will never fall within the threshold
+; due to limited precision in arithmetic operations. This causes infinite loop.
+
+; Auxiliary procedures
+(define (average a b) (/ (+ a b) 2))
+(define (square x) (* x x))
+
+; Improved Newton's method for square roots
+(define (sqrt x)
+  (define (good-enough? prev next)
+    (< (abs (- prev next)) 0.0001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter prev next)
+    (if (good-enough? prev next)
+        next
+        (sqrt-iter next (improve next))))
+  (sqrt-iter 0.0 1.0))
