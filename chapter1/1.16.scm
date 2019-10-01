50349468478b9605aecc864ba5934edd0f5db077
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-10-01 14:03:04 +0200

    Add 1.16

---
+++ chapter1/1.16.scm
@@ -0,0 +1,12 @@
+; Fast exponentiation
+
+(define (square x) (* x x))
+
+(define (expt-iter b n)
+  (define (iter a b n)
+    (cond ((= n 0) a)
+          ((even? n) (iter a (square b) (/ n 2)))
+          (else (iter (* a b) b (- n 1)))))
+  (iter 1 b n))
+
+(expt-iter 3 3)
