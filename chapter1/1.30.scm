e22a677a850da5ee5ed867c3d03046acd50b1c2c
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-10-09 20:42:52 +0200

    Add 1.30

---
+++ chapter1/1.30.scm
@@ -0,0 +1,8 @@
+; Sum procedure, redefined as linear iteration
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+    (iter a 0))
