d9a389f13743f8af5832de82ec29b72076e86e35
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-10-23 23:06:49 +0200

    Redefine repeated in terms of compose

--- chapter1/1.43.scm
+++ chapter1/1.43.scm
@@ -2,11 +2,12 @@
 
 (define (square x) (* x x))
 
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
 (define (repeated f n)
-  (define (repeat g i)
-    (if (= i 0)
-        g
-        (repeat (f g) (dec i))))
-  (lambda (x) (repeat x n)))
+  (if (= n 1)
+      f
+      (compose f (repeated f (dec n)))))
 
 ((repeated square 2) 5)
