57abcdaf9f45ec28719eae81fb41af991f8a8d07
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-11-01 22:47:01 +0100

    Add 2.01

---
+++ chapter2/2.01.scm
@@ -0,0 +1,42 @@
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (numer y) (denom x))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (let ((num (/ n g))
+          (den (/ d g)))
+      (if (< den 0) (cons (- num) (- den))
+          (cons num den)))))
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
